4a893918b31107b7e53c754e408a124b0b9ca041
gunnner <ctrlgrand@gmail.com>
2021-05-16 21:20:13 +0300

    correct typo in password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_05_15_135114) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_disgest"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
